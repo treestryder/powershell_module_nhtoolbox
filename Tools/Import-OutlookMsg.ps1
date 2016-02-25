69554de948fb4320b9cce528b5092dba668b9d04
Nathan Hartley <3682967+treestryder@users.noreply.github.com>
2016-02-25 16:33:07 -0500

    Added Import-OutlookMsg, which imports Outlook MSG files.

---
+++ Tools/Import-OutlookMsg.ps1
@@ -0,0 +1,42 @@
+﻿function Import-OutlookMsg {
+<#
+.Synopsis
+   Imports Outlook MSG export formated files.
+
+.Notes
+    Requires Outlook to be installed.
+
+.Example
+    Import-OutlookMsg -Path 'C:\Users\nhartley\Downloads\DANGER\SpearPhishingAttempt-Attached_Image.msg'
+
+#>
+
+    [CmdletBinding()]
+    Param (
+        # Path or paths to MSG file to import. Excepts piped input.
+        [Parameter(Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            Position=0)]
+        [string[]]$Path
+    )
+
+    Begin {
+        Add-Type -assembly "Microsoft.Office.Interop.Outlook"
+        $Outlook = New-Object -TypeName Microsoft.Office.Interop.Outlook.ApplicationClass
+        $Mapi = $Outlook.GetNameSpace("MAPI")
+    }
+    Process {
+        foreach ($file in $Path) {
+            $MsgObject = $Mapi.Session.OpenSharedItem($file)
+            Write-Output $MsgObject
+            $MsgObject.Close([Microsoft.Office.Interop.Outlook.OlInspectorClose]::olDiscard)
+        }
+    }
+
+    End {
+        $MsgObject = $null
+        $Mapi = $null
+        $Outlook = $null
+    }
+}
