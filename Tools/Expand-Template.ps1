1485cb0c3dc84516c077b6316b626a2d10ccb201
Nathan Hartley <3682967+treestryder@users.noreply.github.com>
2016-09-12 13:59:08 -0400

    Expand-Template now accepts piped input and objects, for the values.

--- Tools/Expand-Template.ps1
+++ Tools/Expand-Template.ps1
@@ -4,7 +4,7 @@
 Processes a string as a template.
 
 .Description
-Uses the .Net format syntax, after first converting any matching keys in a hash table to their respective index value.
+Uses the .Net format syntax, after first converting any matching keys in a hash table or properties in an object to their respective index value.
 
 .Example
 Expand-Template -Value @{a=1} -Template 'a = {a}'
@@ -24,24 +24,52 @@ Expand-Template -Template ' {decimal:0.0#} {stringName,20} ' -Value @{ stringNam
 
  08/05/2016 stringValue
 
-.Notes
-TODO: Handle piped Value input, including objects.
-
 #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true)]
-        [hashtable]$Value,
+        [Parameter(Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName='Hashtable')]
+        [hashtable[]]$Hashtable,
+        [Parameter(Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName='PSObject')]
+        [PSObject[]]$InputObject,
         [Parameter(Mandatory=$true)]
         [string]$Template
     )
-    $i = 0
-    foreach ($key in $Value.Keys) {
-        $TokenRegex = "\{$key(\}|[\,\:][^\}]+\})"
-        $Template = $Template -replace $TokenRegex, "{$i`$1"
-        $i++
-        Write-Verbose "Regex:    $TokenRegex"
-        Write-Verbose "Template: $Template"
+    
+    begin {
+        function ProcessHashtable {
+            param (
+                [hashtable]$Hashtable,
+                [string]$Template
+            )
+            $i = 0
+            foreach ($key in $Hashtable.Keys) {
+                $TokenRegex = "\{$key(\}|[\,\:][^\}]+\})"
+                $Template = $Template -replace $TokenRegex, "{$i`$1"
+                $i++
+                Write-Verbose "Regex:    $TokenRegex"
+                Write-Verbose "Template: $Template"
+            }
+            return $Template -f [array]$Hashtable.Values
+        }
     }
-    $Template -f [array]$Value.Values
+
+    process {
+        foreach ($h in $Hashtable) {
+            ProcessHashtable -Hashtable $h -Template $Template
+        } 
+
+        foreach ($o in $InputObject) {
+            $ht = @{}
+            $o.psobject.Properties | foreach {
+                $ht.Add($_.Name,$_.Value)
+            }
+            ProcessHashtable -Hashtable $ht -Template $Template -verbose
+        }
+    }
 }
